e85a3e5a5fde6dcbb4e53b42d0ac65e12b2cc461
Dh-ux <58239497+Dh-ux@users.noreply.github.com>
2021-10-25 16:53:49 -0700

    lab7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -2,12 +2,34 @@
 * Author: Lexin Xiong <lxiong19@ucsc.edu>
 * Created: 24 October
 */
+
+//code for shuffle from https://www.codespeedy.com/shuffle-characters-of-a-string-in-javascript/
+function shuffleUserName(){
+  userNameshuffle = userName.toLowerCase().split("")
+  userNameshuffle.sort(function(){
+    return 0.5-Math.random(0,1);
+  });
+  userNameshuffle[0] = userNameshuffle[0].toUpperCase();
+  shuffledName = userNameshuffle.join("");
+  return shuffledName
+};
+//function shuffleUserName(){
+  //shuffle = ("")
+  //userNameshuffle = userName.split("")
+  //for (i = userName.length; i--; ) {
+    //random = userNameshuffle.splice(Math.floor(Math.random() * (i + 1)), 1)
+    //shuffle=shuffle.concat(random)
+//}
+  //return shuffle
+//}
 function sortUserName() {
   //Code for remove space from a string come from https://www.geeksforgeeks.org/how-to-remove-spaces-from-a-string-using-javascript/
-  userName = userName.toLowerCase().replace(/ /g, "").split("");
-  var sortedName = userName.sort().join("");
+  userNamelower = userName.toLowerCase().replace(/ /g, "").split("");
+  sortedName = userNamelower.sort().join("");
   return sortedName;
 }
+
 var userName = prompt("Hi. Please tell me your name so I can fix it.", "");
+
 document.writeln("Oh hey, I've fixed your name: ", sortUserName(),"</br>");
-document.writeln(namelength)
+document.writeln("Oh hey, I've shuffled your name: ", shuffleUserName(),"</br>")
