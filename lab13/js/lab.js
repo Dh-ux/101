a8e9f0f57f5adda3257038ed3a96591151bc76ef
Dh-ux <58239497+Dh-ux@users.noreply.github.com>
2021-11-22 16:00:32 -0800

    lab 13 revise

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -7,41 +7,42 @@
 function fizzBuzz() {
   var oneLongStr = "";
   for(var i = 1; i <= 200; i++) {
+
     if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0 && i % 11 == 0) {
-      oneLongStr += i + " FizzBuzzBoomBang!<br>";
+      oneLongStr += i + ": FizzBuzzBoomBang!<br>";
     }
     else if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
-      oneLongStr += i + " FizzBuzzBoom!<br>";
+      oneLongStr += i + ": FizzBuzzBoom!<br>";
     }
     else if (i % 3 == 0 && i % 5 == 0) {
-      oneLongStr += i + " FizzBuzz!<br>";
+      oneLongStr += i + ": FizzBuzz!<br>";
     }
     else if (i % 3 == 0 && i % 7 == 0) {
-      oneLongStr += i + " FizzBoom!<br>";
+      oneLongStr += i + ": FizzBoom!<br>";
     }
     else if (i % 3 == 0 && i % 11 == 0) {
-      oneLongStr += i + " FizzBang!<br>";
+      oneLongStr += i + ": FizzBang!<br>";
     }
     else if (i % 5 == 0 && i % 7 == 0) {
-      oneLongStr += i + " BuzzBoom!<br>";
+      oneLongStr += i + ": BuzzBoom!<br>";
     }
     else if (i % 5 == 0 && i % 11 == 0) {
-      oneLongStr += i + " BuzzBang!<br>";
+      oneLongStr += i + ": BuzzBang!<br>";
     }
     else if (i % 7 == 0 && i % 11 == 0) {
-      oneLongStr += i + " BoomBang!<br>";
+      oneLongStr += i + ": BoomBang!<br>";
     }
     else if (i % 3 == 0) {
-      oneLongStr += i + " Fizz!<br>";
+      oneLongStr += i + ": Fizz!<br>";
     }
     else if (i % 5 == 0) {
-      oneLongStr += i + " Buzz!<br>";
+      oneLongStr += i + ": Buzz!<br>";
     }
     else if (i % 7 == 0) {
-      oneLongStr += i + " Boom!<br>";
+      oneLongStr += i + ": Boom!<br>";
     }
     else if (i % 11 == 0) {
-      oneLongStr += i + " Bang!<br>";
+      oneLongStr += i + ": Bang!<br>";
     }
     else {
       oneLongStr += i + "<br>";
